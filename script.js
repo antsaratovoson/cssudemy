79df54522d92119532c8c2fffd646721dadecd35
Antsa Ratovoson <aratovoson@odity.fr>
2022-05-31 17:52:27 +0300

    ajout transition

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('#btn-animation').addEventListener('click', () => {
+    document.querySelector('.box').classList.toggle('animation');
+     document.querySelector('.paragraphe-animation').classList.toggle('animation');
+})
