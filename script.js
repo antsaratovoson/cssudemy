cce9b894d8b1058384d82656dc2589bb8e520aa7
Antsa Ratovoson <aratovoson@odity.fr>
2022-06-01 16:48:08 +0300

    ajout hamberger

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-document.querySelector('#btn-animation').addEventListener('click', () => {
+const toggleBox = () => {
     document.querySelector('.box').classList.toggle('animation');
-     document.querySelector('.paragraphe-animation').classList.toggle('animation');
-})
+    document.querySelector('.paragraphe-animation').classList.toggle('animation');
+}
+document.querySelector('#btn-animation').addEventListener('click', toggleBox);
+
+
+const btnActiveHamburger = document.querySelector('.btn-hamburger');
+btnActiveHamburger.addEventListener('click', function(){
+    this.classList.toggle('active');
+});
